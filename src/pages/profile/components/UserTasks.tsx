fc75ba6faace7a8e89d86fced05550e269ef5ebf
AbuZhuma <abdirahman.zhumagulov@gmail.com>
2025-08-09 22:28:13 +0600

    save

--- src/pages/profile/components/UserTasks.tsx
+++ src/pages/profile/components/UserTasks.tsx
@@ -1,12 +1,13 @@
 import CreateComment from "@/pages/crm/components/CreateComment"
 import userStore from "@/store/userStore"
 import DataTable from "@/widgets/data_table/DataTable"
+import React from "react"
 
-const UserTasks = () => {
+const UserTasks = React.memo(() => {
       const {user} = userStore()
       return (
             <DataTable form={(item) => <CreateComment data={item}/>} title={`${user.username} active tasks`} data={user.tasks}/>
       )
-}
+})
 
 export default UserTasks
